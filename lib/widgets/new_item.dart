177b44249cb958f2cd16e76285a847297b9e4c64
weali05 <alfredwemali05@gmail.com>
2024-12-29 13:59:25 +0300

    form aware dropdown button

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatelessWidget {
   const NewItem({super.key});
@@ -16,13 +17,50 @@ class NewItem extends StatelessWidget {
             children: [
               TextFormField(
                 maxLength: 50,
-                decoration: const InputDecoration(
-                  label: Text('Name')
-                ),
+                decoration: const InputDecoration(label: Text('Name')),
                 validator: (value) {
                   return 'Demo';
                 },
-              ),//instead of TextField
+              ), //instead of TextField
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                              value: category.value,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 16,
+                                    height: 16,
+                                    color: category.value.color,
+                                  ),
+                                  const SizedBox(
+                                    width: 6,
+                                  ),
+                                  Text(category.value.title)
+                                ],
+                              )),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
