faeb7192de1304261f3f7b3945e95ff7c353d9b0
weali05 <alfredwemali05@gmail.com>
2024-12-30 15:48:24 +0300

    sending post requestto backend

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
@@ -22,12 +24,19 @@ class _NewItemState extends State<NewItem> {
     void _saveItem() {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        Navigator.of(context).pop(
-          GroceryItem(
-              id: DateTime.now().toString(),
-              name: _enteredName,
-              quantity: _enteredQuantity,
-              category: _selectedCategory),
+        final url = Uri.https('flutter-prep-e946b-default-rtdb.firebaseio.com',
+            'shopping-list.json');
+
+        http.post(
+          url,
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode(
+            {
+              'name': _enteredName,
+              'quantity': _enteredQuantity,
+              'category': _selectedCategory.title
+            },
+          ),
         );
       }
     }
@@ -107,7 +116,7 @@ class _NewItemState extends State<NewItem> {
                       ],
                       onChanged: (value) {
                         // setState(() {
-                          _selectedCategory = value!;
+                        _selectedCategory = value!;
                         // });
                       },
                     ),
