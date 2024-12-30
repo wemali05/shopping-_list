c57cd27dba58e9b5f70c14298ac9bb0814317e55
weali05 <alfredwemali05@gmail.com>
2024-12-30 16:34:43 +0300

    fetching and transforming data

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 // import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -11,7 +15,41 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _gorceryItems = [];
+  List<GroceryItem> _gorceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-e946b-default-rtdb.firebaseio.com', 'shopping-list.json');
+
+    final response = await http.get(url);
+    final Map<String, dynamic> listData =
+        json.decode(response.body);
+    final List<GroceryItem> _loadItems = [];
+
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      _loadItems.add(
+        GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category),
+      );
+    }
+
+    setState(() {
+      _gorceryItems = _loadItems;
+    });
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
@@ -20,13 +58,7 @@ class _GroceryListState extends State<GroceryList> {
       ),
     );
 
-    if (newItem == null) {
-      return;
-    }
-
-    setState(() {
-      _gorceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   @override
@@ -39,7 +71,7 @@ class _GroceryListState extends State<GroceryList> {
       });
     }
 
-    if(_gorceryItems.isNotEmpty) {
+    if (_gorceryItems.isNotEmpty) {
       content = ListView.builder(
         itemCount: _gorceryItems.length,
         itemBuilder: (ctx, index) => Dismissible(
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -39,13 +39,10 @@ class _NewItemState extends State<NewItem> {
           ),
         );
 
-        print(response.body);
-        print(response.statusCode);
-
         if(!context.mounted) {
           return;
         }
-        
+
         Navigator.of(context).pop();
       }
     }
