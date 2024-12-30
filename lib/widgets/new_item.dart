28829e5fdb545d13d9d4842c73091a6f584819a7
weali05 <alfredwemali05@gmail.com>
2024-12-30 15:28:19 +0300

    add http package

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
